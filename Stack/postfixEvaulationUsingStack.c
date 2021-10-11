70a6ded268f4fb7f3f674bbd3170b1481d17d8d1
Ashutosh Uniyal <83587446+Ashuuniyal12@users.noreply.github.com>
2021-10-11 19:44:37 +0530

    Create postfixEvaulationUsingStack.c

---
+++ Stack/postfixEvaulationUsingStack.c
@@ -0,0 +1,169 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define N 50
+
+int stack[N];
+int top =-1;
+
+void push(char x) 
+{
+    if(top ==N-1)
+        printf("Overflow\n");
+    else
+    {
+        top++;  
+        stack[top]=x;
+    }
+
+}
+
+char pop() 
+{
+    char x =-1;
+    if(top == -1)
+        printf("Underflow");
+    else
+    {
+        x= stack[top];
+        top--;
+    }
+    return x;
+}
+
+int precidence(char x )
+{
+    if(x=='+' || x== '-')
+     return 1;
+    else if(x=='*' || x=='/')
+        return 3;
+    else if(x=='^')
+        return 4;
+    return 0;
+}
+
+int outstack( char x)
+{
+    if(x=='+' || x=='-')
+        return 1;
+    else if(x =='*' || x == '/')
+        return 3;
+    else if(x=='^')
+        return 6;
+    else if(x == '(')
+        return 7;
+    else if(x == ')')
+        return 0;
+
+}
+int instack(char x)
+{
+    if(x=='+' || x=='-')
+        return 2;
+    else if(x =='*' || x == '/')
+        return 4;
+    else if(x=='^')
+        return 5;
+    else if(x=='(')
+        return 0;
+    else if(x == ')')
+        return 0;
+}
+
+int isOperand(char x)
+{
+    if(x=='+' ||x=='-' ||x=='*' ||x=='/'|| x == '(' || x == '^' || x == ')')
+        return 0;
+    else 
+        return 1;
+}
+
+char* InToPsot(char *infix)
+{
+    int i=0,  j =0;
+    char *postfix;
+    int  len = strlen(infix);
+    postfix = (char*)malloc((len+2)*sizeof(char));
+
+    while(infix[i]!='\0')
+    {
+        if(isOperand(infix[i]))
+            postfix[j++] = infix[i++];
+        else
+        {
+            if(outstack(infix[i])> instack(stack[top]))
+            {
+                push(infix[i]);
+                i++;
+            }
+            else
+            {
+                if(outstack(infix[i]) == instack(stack[top]))
+                {
+                   pop();
+                    i++;
+                }
+                
+                else
+                {
+                    postfix[j]=pop();
+                    j++;
+                }
+            }
+        }
+    }
+    while(top>=0)
+    {
+        if(stack[top]!='(' || stack[top]!=')')
+            postfix[j++]= pop();
+    }
+    postfix[j] = '\0';
+    return postfix;
+}
+
+int evaluate(char *postfix)
+{
+    int x1,x2,r;
+    for(int i=0 ;postfix[i]!='\0';i++)
+    {
+        if(isOperand(postfix[i]))
+            push(postfix[i]);
+        else
+        {
+            x2=(int)(pop()-'0'); x1 = (int)(pop()-'0');
+            switch(postfix[i])
+            {
+                case'+':
+                    r= x1+x2;
+                    push((char)(r+48));
+                    break;
+                case'-':
+                    r= x1-x2;
+                    push((char)(r+48));
+                    break;
+                case'*':
+                    r= x1*x2;
+                    push((char)(r+48));
+                    break;
+                case'/':
+                    r= x1/x2;
+                    push((char)(r+48));
+                    break;
+            } 
+        }
+    }
+    return (int)(pop()-'0');
+}
+int main()
+{
+    char str[N] ; 
+    printf("Enter the Expression : ");
+    scanf("%s", &str);
+
+    char *infix =str;
+
+    char *postfix=InToPsot(infix);
+    printf("\nThe postfix Evaluation of Expression is : %s",postfix);
+
+    printf("\nThe result after evaluation of postfix expression is: %d", evaluate(postfix));
+}
